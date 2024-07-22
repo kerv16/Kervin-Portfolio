163184ad95c8977f1111ef2d88318331805807fe
kerv16 <kervtulz@gmail.com>
2024-07-22 08:54:21 +0800

    added new resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/MyResume.pdf';
+import CV from '../../assets/Tulang_KervinNiño_Resume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
